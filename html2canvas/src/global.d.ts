3c3291ea73c34e1f4eac225d3c09ac338fdd6120
sloothes <sloothes.com@gmail.com>
2019-09-23 03:51:12 +0300

    remove html2canvas

--- html2canvas/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-interface CSSStyleDeclaration {
-    textDecorationColor: string | null;
-    textDecorationLine: string | null;
-    overflowWrap: string | null;
-}
-
-interface DocumentType extends Node, ChildNode {
-    readonly internalSubset: string | null;
-}
